82e8bc2d2706e543bc5500b085b6f1c1d529078c
Henry Odiete <henry.odiete@opin.ca>
2019-07-31 22:18:53 -0400

    Challenge Solution for Hobber task

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express'), 
+	request = require('request'),
+	app= express(),
+	fs =require('fs'), 
+	productData = fs.readFileSync(__dirname +'/json/products.json'), //JSON data for products in roster
+	snackersData = fs.readFileSync(__dirname +'/json/snackers.json'), // JSON data for snacks and their emails
+	products= JSON.parse(productData),
+	snackers= JSON.parse(snackersData),
+	totalSnack=0,
+	totalCost=0,
+	port = 3000||process.env.PORT;
+
+app.set ('view engine', 'ejs'); 
+app.use(function(req, res, next){
+  res.locals.totalSnack= totalSnack;// globally access snacks
+  res.locals.totalCost=totalCost;   //globally access cost
+  next();
+})
+
+// //Getting JSON data via url 
+// app.get("/", function (req, res){
+// 	request("https://s3.amazonaws.com/misc-file-snack/MOCK_SNACKER_DATA.json", function (err, response, body){
+//     // console.log('error:', error); // Print the error if one occurred
+//    	if (!err && response.statusCode==200){
+//      var data= JSON.parse(body);
+//      res.render("index", {data: data});
+//     }else{
+//      console.log(err);
+//     }
+
+//     }); 
+// });
+
+//ROUTES
+app.get("/", function(req, res){
+	res.render("index", {data:snackers});
+});
+
+app.get("/shop", function (req, res){
+	res.render("shop", {product:products, snackers:snackers});
+});
+
+app.get("/:id", function(req, res){
+
+	res.send("Sorry Page doesn't exist");
+})
+
+app.listen(port, function (req, res){
+
+	console.log("Server is Working");
+})
---
+++ readme.md
@@ -0,0 +1,37 @@
+
+#Challenge Solution#
+
+`Step 1`. Solve this:
+
+Use the following fake snacker list:
+
+https://s3.amazonaws.com/misc-file-snack/MOCK_SNACKER_DATA.json
+
+Find all emails of snackers with a 'fave_snack'of a product we stock:
+
+Our product list can be found here (assume all products here are in stock):
+https://ca.desknibbles.com/products.json?limit=250
+
+a) List the real stocked snacks you found under the snacker's 'fave_snack'?
+b) What're the emails of the snackers who listed those as a 'fave_snack'?
+c) If all those snackers we're to pay for their 'fave_snack'what's the total price?
+
+`Step 2`. Apply: 
+Answer question abc (optional), and let us know what your favourite snack is!
+
+
+
+#Answer#
+  Tools used 
+  1. Express
+  2. Request
+  3. EJS
+  4. Filesystem module
+
+ The `/index` page shows the number of emails and their fav snacks 
+ The `/shop` page answers a, b, and c.
+
+
+Pipeline build created in heroku available here: 
+
+
